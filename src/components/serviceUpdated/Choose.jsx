9f5f18afb87296ee23fc58eec10b2d2b908fefe6
PIYUSH <bosspiyush98@gmail.com>
2023-04-09 20:56:33 +0530

    service page update

--- src/components/serviceUpdated/Choose.jsx
+++ src/components/serviceUpdated/Choose.jsx
@@ -9,26 +9,26 @@ const Choose = () => {
                 <div className=' w-full flex flex-col gap-20 text-white '>
 
                    <div className=' w-90 flex flex-col gap-5 text-center md:mt-40 '>
-                    <span className=' text-4xl lg:text-5xl font-bold '>why choose us?</span>
-                    <p className='text-center '>Hubnex Lab employs industry best practices to help businesses globally scale, transform, and gain competitive advantage through <br /> innovative software solutions. <br></br> of how we work and what we do. We are:</p>
+                    <span className=' text-4xl lg:text-5xl font-gilroy-bold '>why choose us?</span>
+                    <p className='text-center  font-gilroy'>Hubnex Lab employs industry best practices to help businesses globally scale, transform, and gain competitive advantage through <br /> innovative software solutions. <br /></p>
                    </div>
 
                    <div className=' w-full flex flex-col gap-8 lg:gap-0 lg:flex-row justify-between items-center  '>
                         <div className=' w-80 text-center mb-4 lg:mr-9 '>
-                            <span className=' text-xl font-semibold'>Your Next-Gen Technology <br/> Partner</span>
-                            <p className='text-base font-sans font-thin'><br />Adherence to agile and CI/CD <br />principles throughout the product <br />development lifecycle gain <br />competitive advantage through <br />innovative software solutions.</p>    
+                            <span className=' text-xl font-gilroy-semibold'>Your Next-Gen Technology <br/> Partner</span>
+                            <p className='text-base font-gilroy font-thin'><br />Adherence to agile and CI/CD <br />principles throughout the product <br />development lifecycle gain <br />competitive advantage through <br />innovative software solutions.</p>    
                         </div>
                         <hr className=' w-[50%] md:w-0 h-[50%] border-[1px]'/>
                        
                         <div className='w-80 text-center mb-4 lg:mr-9 '>
-                            <span className=' text-xl font-semibold '>Team-Oriented</span>
-                            <p className='text-base font-sans font-thin'><br />Efficient & transparent <br />development, communication, and<br /> reporting Our domain knowledge,<br /> expertise, and proven<br /> methodologies enable us to create.</p>
+                            <span className=' text-xl font-gilroy-semibold '>Team-Oriented</span>
+                            <p className='text-base font-gilroy font-thin'> <br /><br />Efficient & transparent <br />development, communication, and<br /> reporting Our domain knowledge,<br /> expertise, and proven<br /> methodologies enable us to create.</p>
                         </div>
                         <hr className=' w-[50%] md:w-0 h-[50%] border-[1px]'/>
 
                         <div className='w-80 text-center  mb-4 lg:mr-9 max-[360px]:w-60'>
-                            <span className=' text-xl font-semibold'>Client-Focused</span>
-                            <p className=' text-base font-sans font-thin'><br />Guaranteed maintenance and <br />after-sales support smart digital<br /> experiences that add value to <br />diverse businesses.</p>
+                            <span className=' text-xl font-gilroy-semibold'>Client-Focused</span>
+                            <p className=' text-base font-gilroy font-thin'> <br /><br />Guaranteed maintenance and <br />after-sales support smart digital<br /> experiences that add value to <br />diverse businesses.</p>
                         </div>
                    </div>
                 </div>
--- src/components/serviceUpdated/Ourservice.jsx
+++ src/components/serviceUpdated/Ourservice.jsx
@@ -7,7 +7,8 @@ import testImg5 from '../../assets/Mobile Development.png';
 import testImg6 from '../../assets/Group.png';
 import testImg7 from '../../assets/API Programming.png';
 import testImg8 from '../../assets/Ux Design.png';
-import testImg9 from '../../assets/Communication.png';
+import testImg9 from '../../assets/Address marker.png';
+import './Ourservice.css'
 
 
 
@@ -25,105 +26,105 @@ const Ourservice = () => {
                 
                 <div className='  flex flex-wrap w-full md:justify-between justify-center gap-5 '>
 
-                <div className=' h-52 w-80 px-5 relative rounded-md bg-brown-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
+                <div className=' card h-52 w-80 px-5 relative rounded-md bg-brown-100 bg-opacity-20  border-500 border-2  flex flex-col justify-evenly '>
                   <span className=' absolute right-5 top-2 text-5xl text-gray-400 font-gilroy-bold'>01</span>
-                  <div className=' w-10 h-10 rounded-full flex justify-center items-center bg-500'>
+                  <div className='  w-10 h-10 rounded-full flex justify-center items-center bg-500'>
                   <img src={testImg1} alt="" className=' w-full h-full '/>
                   </div>
                   <div className=' w-full flex flex-col'>
-                    <p className='  font-semibold text-white'>Custom Web Development</p>
-                    <p className=' text-xs text-white '><br /> We build customized, intuitive web app  solutions that optimize operations, <br /> accelerate workflows, boost revenue,and <br /> deliver an exceptional product experience.</p>
+                    <p className='   font-inter-bold text-white'>Custom Web Development</p>
+                    <p className=' size font-gilroy  text-white '><br /> We build customized, intuitive web app  solutions that optimize operations, <br /> accelerate workflows, boost revenue, and <br /> deliver an exceptional product experience.</p>
                   </div>
                 </div>
 
-                <div className=' h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
+                <div className=' card h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
                   <span className=' absolute right-5 top-2 text-5xl text-gray-400 font-gilroy-bold'>02</span>
                   <div className=' w-10 h-10 rounded-full flex justify-center items-center bg-500'>
                   <img src={testImg2} alt="" className=' w-full h-full '/>
                   </div>
                   <div className=' w-full flex flex-col'>
-                    <p className='  font-semibold text-white'>Software Consulting</p>
-                    <p className=' text-xs text-white  '><br /> We analyze businesses needs and create a <br /> roadmap for building a software solution <br /> optimized to specific requirements and <br /> delivering the best value.</p>
+                    <p className='  font-inter-bold text-white'>Software Consulting</p>
+                    <p className=' size font-gilroy text-white  '><br /> We analyze businesses needs and create a <br /> roadmap for building a software solution <br /> optimized to specific requirements and <br /> delivering the best value.</p>
                   </div>
                 </div>
                 
 
-                <div className=' h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
+                <div className='card h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
                   <span className=' absolute right-5 top-2 text-5xl text-gray-400 font-gilroy-bold'>03</span>
                   <div className=' w-10 h-10 rounded-full flex justify-center items-center bg--500'>
                   <img src={testImg3} alt="" className=' w-full h-full rounded-full'/>
                   </div>
                   <div className=' w-full flex flex-col'>
-                    <span className='  font-semibold text-white'>Implementation & Deployment</span>
-                    <p className=' text-xs text-white  '><br /> We device comprehensive and agile <br /> deployment & implementation plans using proven methodologies to deliver enhanced and cost-effective solutions to end-users.</p>
+                    <span className='  font-inter-bold text-white'>Implementation & Deployment</span>
+                    <p className=' size font-gilroy text-white  '><br /> We device comprehensive and agile <br /> deployment & implementation plans using proven methodologies to deliver enhanced and cost-effective solutions to end-users.</p>
                   </div>
                 </div>
 
-                <div className=' h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
+                <div className='card h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
                   <span className=' absolute right-5 top-2 text-5xl text-gray-400 font-gilroy-bold'>04</span>
                   <div className=' w-10 h-10 rounded-full flex justify-center items-center bg--500'>
                   <img src={testImg4} alt="" className=' w-full h-full rounded-full'/>
                   </div>
                   <div className=' w-full flex flex-col'>
-                    <pre className='  font-semibold text-white'>Maintenance & Management</pre>
-                    <p className=' text-xs text-white  '><br /> Our app maintenance and management <br /> services ensure that your software <br /> infrastructure stays relevant, scalable, <br /> sustainable, and high-performing. </p>
+                    <pre className='  font-inter-bold text-white'>Maintenance & Management</pre>
+                    <p className=' size font-gilroy text-white  '><br /> Our app maintenance and management <br /> services ensure that your software <br /> infrastructure stays relevant, scalable, <br /> sustainable, and high-performing. </p>
                   </div>
                 </div>
 
-                <div className=' h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
+                <div className='card h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
                   <span className=' absolute right-5 top-2 text-5xl text-gray-400 font-gilroy-bold'>05</span>
                   <div className=' w-10 h-10 rounded-full flex justify-center items-center bg--500'>
                   <img src={testImg5} alt="" className=' w-full h-full rounded-full'/>
                   </div>
                   <div className=' w-full flex flex-col'>
-                    <p className='  font-semibold text-white'>Mobile Web Development</p>
-                    <p className=' text-xs text-white  '><br /> Our robust & customizable frameworks help<br /> develop smart ,mobile apps that analyze <br />user data and incorporate AI ML to deliver <br />personalized user experiences. </p>
+                    <p className='  font-inter-bold text-white'>Mobile Web Development</p>
+                    <p className=' size font-gilroy text-white  '><br /> Our robust & customizable frameworks help<br /> develop smart ,mobile apps that analyze <br />user data and incorporate AI ML to deliver <br />personalized user experiences. </p>
                   </div>
                 </div>
                 
 
-                <div className=' h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
+                <div className='card h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
                   <span className=' absolute right-5 top-2 text-5xl text-gray-400 font-gilroy-bold'>06</span>
                   <div className=' w-10 h-10 rounded-full flex justify-center items-center bg--500'>
                   <img src={testImg6} alt="" className=' w-full h-full rounded-full'/>
                   </div>
                   <div className=' w-full flex flex-col'>
-                    <p className='  font-semibold text-white'>Custom Application Development</p>
-                    <p className=' text-xs text-white  '><br /> Leverage our deep domain expertise to<br /> develop robust,scalable & interoperable <br /> mobile, web, desktop & hybrid applications <br /> that deliver optimal business outcomes. </p>
+                    <p className='  font-inter-bold text-white'>Custom Application Development</p>
+                    <p className=' size font-gilroy text-white  '><br /> Leverage our deep domain expertise to<br /> develop robust,scalable & interoperable <br /> mobile, web, desktop & hybrid applications <br /> that deliver optimal business outcomes. </p>
                   </div>
                 </div>
 
-                <div className=' h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
+                <div className='card h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
                   <span className=' absolute right-5 top-2 text-5xl text-gray-400 font-gilroy-bold'>07</span>
                   <div className=' w-10 h-10 rounded-full flex justify-center items-center bg--500'>
                   <img src={testImg7} alt="" className=' w-full h-full rounded-full'/>
                   </div>
                   <div className=' w-full flex flex-col'>
-                    <p className='  font-semibold text-white'>API Development & Integration</p>
-                    <p className=' text-xs text-white  '><br /> Develop reliable and easy to consume<br /> custom APIs that seamlessly integrate <br />custom software with enterprise and third-<br /> party systems & services to maximize uptime <br /> & scalability</p>
+                    <p className='  font-inter-bold text-white'>API Development & Integration</p>
+                    <p className=' size font-gilroy text-white  '><br /> Develop reliable and easy to consume<br /> custom APIs that seamlessly integrate <br />custom software with enterprise and third-<br /> party systems & services to maximize uptime <br /> & scalability</p>
                   </div>
                 </div>
 
-                <div className=' h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
+                <div className='card h-52 relative w-80 px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
                   <span className=' absolute right-5 top-2 text-5xl text-gray-400 font-gilroy-bold'>08</span>
                   <div className=' w-10 h-10 rounded-full flex justify-center items-center bg--500'>
                   <img src={testImg8} alt="" className=' w-full h-full rounded-full'/>
                   </div>
                   <div className=' w-full flex flex-col'>
-                    <p className='  font-semibold text-white'>Custom UX/UI Design</p>
-                    <p className=' text-xs text-white  '><br />Leverage the latest UX/UI technologies, <br />architectures, and trends to design <br />responsive & scalable user-centric apps <br />that transform user experiences across <br />channels.</p>
+                    <p className='  font-inter-bold text-white'>Custom UX/UI Design</p>
+                    <p className=' size font-gilroy text-white  '><br />Leverage the latest UX/UI technologies, <br />architectures, and trends to design <br />responsive & scalable user-centric apps <br />that transform user experiences across <br />channels.</p>
                   </div>
                 </div>
                 
 
-                <div className=' h-52 w-80 relative px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
+                <div className='card  h-52 w-80 relative px-5 rounded-md bg-black-100 bg-opacity-20  border-blue-500 border-2  flex flex-col justify-evenly '>
                   <span className=' absolute right-5 top-2 text-5xl text-gray-400 font-gilroy-bold'>09</span>
                   <div className=' w-10 h-10 rounded-full flex justify-center items-center bg--500'>
                   <img src={testImg9} alt="" className=' w-full h-full rounded-full'/>
                   </div>
                   <div className=' w-full flex flex-col'>
-                    <p className='  font-semibold text-white'>System Integration</p>
-                    <p className=' text-xs text-white  '><br /> Our Software integration engineers adopt <br />next-gen technologies and process to <br /> streamline and integrate everything from <br /> design, development, and testing to execution.</p>
+                    <p className='  font-inter-bold text-white'>System Integration</p>
+                    <p className=' size font-gilroy text-white  '><br /> Our Software integration engineers adopt <br />next-gen technologies and process to <br /> streamline and integrate everything from <br /> design, development, and testing to execution.</p>
                   </div>
                 </div>
               </div>
